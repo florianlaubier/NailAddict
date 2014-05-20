212e8add2d8fe539f5d94de3b2a701a100635216
MatheaR <mathea.renaudie@gmail.com>
2014-05-20 22:36:41 +0200

    page d'inscription presque fonctionnelle

--- www/detail-inscription.php
+++ www/detail-inscription.php
@@ -1,75 +1,13 @@
 <?php
   require_once("header.php");
-  require_once("connexion.php");
-
-  if(isset($_POST) && is_array($_POST) && isset($_POST['envoi_inscription']))
-  {
-  echo "ta mere";
-    $errors = array();
-
-    if(!isset($_POST['nom']) || $_POST['nom'] == '')
-    {
-      $errors[] = "Nom obligatoire !";
-    }
-    if(!isset($_POST['prenom']) || $_POST['prenom'] == '')
-    {
-      $errors[] = "Prenom obligatoire !";
-    }
-    if(!isset($_POST['dateNaissance']) || $_POST['dateNaissance'] == '')
-    {
-      $errors[] = "la date de naissance est obligatoire !";
-    }
-    if(!isset($_POST['pseudo']) || $_POST['pseudo'] == '')
-    {
-      $errors[] = "Pseudo obligatoire !";
-    }
-    if(!isset($_POST['password']) || $_POST['password'] == '' || !isset($_POST['verifMdp']) || $_POST['verifMdp'] == '')
-    {
-      $errors[] = "Mot de passe obligatoire !";
-    }
-    if($_POST['verifMdp'] != $_POST['password'])
-    {
-      $errors[] = "Mot de passe et Vérification doivent être identique!";
-    }
-    if(!isset($_POST['ville']) || $_POST['ville'] == '')
-    {
-      $errors[] = "Ville obligatoire !";
-    }
-
-    foreach($errors as $error)
-    {
-      echo $error, '<br/>';
-    }
-    if(count($errors) == 0)
-    {
-
-      mysql_connect($bdd_server, $bdd_user, $bdd_pass) or die(mysql_error());
-      mysql_select_db($bdd_name) or die(mysql_error());
-
-      mysql_query("INSERT INTO `nail`.`utilisateur`
-                  (`id_user`, `nom`, `prenom`, `pseudo`, `mot_de_passe`,
-                   `date_naissance`, `lien_photo`, `description_user`, `id_localisation_user`)
-            VALUES (NULL, 'flo', 'flo', 'flo', 'flo', '1991-05-30', NULL, NULL, NULL)")
-            or die(mysql_error());
-
-
-      // mysql_query("INSERT INTO eleves (nom,prenom,niveau_division,sexe,note_globale,note_comp,div_anterieur)
-      //         VALUES ('" . $_POST['nom'] . "', '" . $_POST['prenom'] . "', '" . $_POST['niveau_division'] . "','" . $_POST['sexe'] . "',
-      //             '" . $_POST['note_globale'] . "','" . $_POST['note_comp'] . "','" . $_POST['div_anterieur'] . "')")
-      //             or die(mysql_error());
-
-      echo("Vous êtes bien inscrit.");
-    }
-  }
-?>
-
+  require_once("connexion.php"); ?>
 
 <body ng-app="starter" class="padding-vertical">
   <ion-pane>
 
     <!-- Cacher pour enlever la bar de titre -->
     <ion-header-bar class="bar-stable">
-      <h1 class="title">Inscrption</h1>
+      <h1 class="title">Inscription</h1>
     </ion-header-bar>
 
     <ion-content class="index">
@@ -77,7 +15,7 @@
 
       <div class="list" style="text-align:left;">
 
-        <form method=post action="#">
+        <form method="post" action="#">
 
           <label class="item item-input">
             <span class="input-label">Nom</span>
@@ -91,7 +29,7 @@
 
           <label class="item item-input">
             <span class="input-label">Date de naissance</span>
-            <input id="dateNaissance" name="dateNaissance" type="text">
+            <input id="dateNaissance" name="dateNaissance" type="date">
           </label>
 
           <label class="item item-input">
@@ -106,12 +44,12 @@
 
           <label class="item item-input">
             <span class="input-label">Vérification MDP</span>
-            <input id="verifMdp" name="nom" type="password">
+            <input id="verifMdp" name="verifMdp" type="password">
           </label>
 
           <label class="item item-input">
             <span class="input-label">Ville</span>
-            <input id="ville" name="nom" type="text">
+            <input id="ville" name="ville" type="text">
           </label>
 
 <!--           <label class="item item-input">
@@ -121,14 +59,83 @@
 
           <label class="item item-input">
             <span class="input-label">Description</span>
-            <textarea id="descripUser" name="nom" rows="10"></textarea>
+            <textarea id="descripUser" name="description" rows="10"></textarea>
           </label>
+
+          <?php
+
+  if(isset($_POST) && is_array($_POST) && isset($_POST['envoi_inscription']))
+  {
+  
+    $errors = array();
+
+    $nom = mysql_real_escape_string(htmlspecialchars($_POST['nom']));
+    $prenom = mysql_real_escape_string(htmlspecialchars($_POST['prenom']));
+    $pseudo = mysql_real_escape_string(htmlspecialchars($_POST['pseudo']));
+    $passe = mysql_real_escape_string(htmlspecialchars($_POST['password']));
+    $passe2 = mysql_real_escape_string(htmlspecialchars($_POST['verifMdp']));
+
+    if(isset($_POST['ville']) || $_POST['ville'] != '')
+    {
+      $ville = mysql_real_escape_string(htmlspecialchars($_POST['ville']));
+    }
+    if(isset($_POST['description']) || $_POST['description'] != '')
+    {
+      $description = mysql_real_escape_string(htmlspecialchars($_POST['description']));
+    }
+    if(!isset($nom) || $nom == '')
+    {
+      $errors[] = "Nom obligatoire !";
+    }
+    if(!isset($prenom) || $prenom == '')
+    {
+      $errors[] = "Prénom obligatoire !";
+    }
+    if(!isset($_POST['dateNaissance']) || $_POST['dateNaissance'] == '')
+    {
+      $errors[] = "Date de naissance est obligatoire !";
+    }
+    if(!isset($pseudo) || $pseudo == '')
+    {
+      $errors[] = "Pseudo obligatoire !";
+    }
+    if(!isset($passe) || $passe == '' || !isset($passe2) || $passe2 == '')
+    {
+      $errors[] = "Mot de passe obligatoire !";
+    }
+    if($passe != $passe2)
+    {
+      $errors[] = "Les mots de passes doivent être identiques !";
+    }
+
+    foreach($errors as $error)
+    {
+      echo $error, '<br/>';
+    }
+    if(count($errors) == 0)
+    {
+
+      mysql_connect($bdd_server, $bdd_user, $bdd_pass) or die(mysql_error());
+      mysql_select_db($bdd_name) or die(mysql_error());
+
+      $passCrypt = sha1($passe);
+
+      mysql_query("INSERT INTO `nail`.`utilisateur`
+                  (`id_user`, `nom`, `prenom`, `pseudo`, `mot_de_passe`,
+                   `date_naissance`, `lien_photo`, `description_user`, `id_localisation_user`)
+            VALUES (NULL, '" . $_POST['nom'] . "', '" . $_POST['prenom'] . "', '" . $_POST['pseudo'] . "', '" . $passCrypt . "', 
+              '" . $_POST['dateNaissance'] . "', NULL, NULL, NULL)")
+            or die(mysql_error());
+
+      echo("Vous êtes bien inscrit(e).");
+    }
+  }
+?>
+
+          <input class="button button-block button-energized" name="envoi_inscription" type="submit" value="Envoi"/>
 
         </form>
       </div>
-
-      <input class="button button-block button-energized" name="envoi_inscription" type="submit" value="Envoi"/>
-
       <div class="space-tab"></div>
     </ion-content>
 
